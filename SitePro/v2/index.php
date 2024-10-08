e5a3901b31fea9c7435f29f96ddb7a3774677bbc
Zhensheng LIU <liuzhensheng926@gmail.com>
2024-10-08 18:36:08 +0200

    enter

--- SitePro/v2/index.php
+++ SitePro/v2/index.php
@@ -9,7 +9,7 @@ require_once('template_header.php');
     <!-- menu end -->
     <body class="index-body">
     <!-- content begin -->
-    <h1 class="index-p">Qui suis-je?</h1>
+    <h1 class="index-h1">Qui suis-je?</h1>
     <em style="font-size:20px;color:red;">A smart cat</em>
     <p class="index-p" style="text-align:center">Feed me a question and i will give u an answer</p>
     <div class="search-container">
--- SitePro/v2/template_header.php
+++ SitePro/v2/template_header.php
@@ -4,5 +4,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ZhenshengLIU</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
