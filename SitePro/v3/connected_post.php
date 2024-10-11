35d2583bc4d84fd624b8aabb034fc84b2190f220
Zhensheng LIU <liuzhensheng926@gmail.com>
2024-10-11 08:51:38 +0200

    enter

--- SitePro/v3/connected_post.php
+++ SitePro/v3/connected_post.php
@@ -4,7 +4,16 @@ session_start();
 $login = isset($_POST['login']) ? $_POST['login'] : '';
 $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-if ($login == 'admin' && $password == '1234567') {
+if ($login == 'admin' && $password == '123456') {
+
+    $_SESSION['login'] = $login;
+    echo "<br>Bienvenue, " . htmlspecialchars($login) . "! Vous êtes connecté.";
+    echo "<br><a href='index.php'>Aller à l'accueil</a>";
+} else {
+    echo "<br>Login ou mot de passe incorrect.";
+}
+
+if ($login == 'admin2' && $password == '123456') {
 
     $_SESSION['login'] = $login;
     echo "<br>Bienvenue, " . htmlspecialchars($login) . "! Vous êtes connecté.";
