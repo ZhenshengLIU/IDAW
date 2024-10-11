154c728355276a012b763a81d3025d78d5188d7c
Zhensheng LIU <liuzhensheng926@gmail.com>
2024-10-11 08:15:51 +0200

    enter

--- SitePro/v2/connected_post.php
+++ SitePro/v2/connected_post.php
@@ -1,14 +1,4 @@
 <?php
-
-$login = isset($_POST['login']) ? $_POST['login'] : '';
-$password = isset($_POST['password']) ? $_POST['password'] : '';
-
-
-echo "Login: " . htmlspecialchars($login) . "<br>";
-echo "Mot de passe: " . htmlspecialchars($password);
-?>
-
-<?php
 session_start(); 
 
 $login = isset($_POST['login']) ? $_POST['login'] : '';
