ee164247070c82a2f78bce2d63a0d08efb2c71f0
Zhensheng LIU <liuzhensheng926@gmail.com>
2024-10-14 11:37:30 +0200

    enter

--- tp4/ex3/inti_db.php
+++ tp4/ex3/inti_db.php
@@ -1,39 +1,22 @@
 <?php
 
-require_once('config.php');
-
-$connectionString = "mysql:host=" . _MYSQL_HOST;
-if (defined('_MYSQL_PORT')) {
-    $connectionString .= ";port=" . _MYSQL_PORT;
-}
-
-$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-$pdo = null;
+$host = 'localhost'; 
+$dbname = 'autocreate_db'; 
+$username = 'root'; 
+$password = 'root'; 
 
 try {
 
-    $pdo = new PDO($connectionString, _MYSQL_USER, _MYSQL_PASSWORD, $options);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
 
+ 
     $sql = file_get_contents('create_db.sql');
-    
-    if ($sql === false) {
-        throw new Exception('Cannot read SQL file.');
-    }
-    
 
     $pdo->exec($sql);
     
-    echo "Database successfully initialized.";
-} catch (PDOException $erreur) {
-    echo 'Database connection error: ' . $erreur->getMessage();
-    exit();
-} catch (Exception $e) {
-    echo 'Error: ' . $e->getMessage();
-    exit();
+    echo "create sucessfully";
+} catch (PDOException $e) {
+    echo "create unsucessfully " . $e->getMessage();
 }
-
-
-$pdo = null;
 ?>
